4d2cc278e77dd4a74015de1105a3c830356e9f2b
Kamil Baldyga <baldygakamil@gmail.com>
2025-04-18 00:07:38 +0200

    work in progress

--- public/js/classes.js
+++ public/js/classes.js
@@ -1,11 +1,11 @@
-/**
+/*
 |====================================================================|
 |                                                                    |
 |                            Classes                                 |
 |                                                                    |
 |====================================================================|
  */
-/**
+/*
  * Classe Joueur 
 ●  nom 
 ●  numéro (ex : 10) 
@@ -31,18 +31,24 @@ tir(gardien)
 ●  Sinon : balle au gardien 
 ●  Fatigue +5 
  */
+/**
+ *  attack = Values between 0 - 60 (60 - 90 if attacker);
+    defense = Values between 0 - 60 (60 - 90 if defender);
+    speed = 0 Values between 0 - 60 (60 - 90 if attacker);
+    reflex = 0 Values between 0 - 60 (60 - 90 if goalkeeper);
+ */
 //                      Player
 export class Player {
   constructor(
     name = "String",
     nb = 0,
-    attack = 0, // Values between 0 - 60 (60 - 90 if attacker)
-    defense = 0, // Values between 0 - 60 (60 - 90 if defender)
-    speed = 0, // Values between 0 - 60 (60 - 90 if attacker)
-    reflex = 0, // Values between 0 - 60 (60 - 90 if goalkeeper)
+    attack = 0,
+    defense = 0,
+    speed = 0,
+    reflex = 0,
     foot = [],
     fatigue = 0,
-    hasBall = true
+    hasBall = []
   ) {
     this.name = name;
     this.nb = nb;
@@ -56,33 +62,39 @@ export class Player {
   }
   dribble = (opponent) => {
     if (this.hasBall) {
-      let dribbleAttack = [(this.attack * speed) / 100] - this.fatigue;
+      var dribbleAttack = [(this.attack * this.speed) / 100] - this.fatigue;
       // VS
-      let dribbleDefense = [(this.defense * this.reflex) / 100] - this.fatigue;
+      var dribbleDefense =
+        [(opponent.defense * opponent.reflex) / 100] - opponent.fatigue;
     }
     if (dribbleAttack > dribbleDefense) {
       console.log(`Successful dribble!`);
-    } else {
+      dribbleResult = true;
+    } else if (dribbleAttack < dribbleDefense) {
       console.log(`Unsuccesful dribble!`);
-      opponent.hasBall = true;
+      dribbleResult = false;
+      opponent.hasBall.push("hasBall");
+      this.hasBall.pop();
       this.fatigue += 5;
+    } else if (dribbleAttack == dribbleDefense) {
+      console.log("test");
     }
   };
   shoot = (goalkeeper) => {
     if (this.hasBall) {
-      let attackGoal = [(this.attack * speed) / 100] - this.fatigue;
+      let goalAttack = [(this.attack * this.speed) / 100] - this.fatigue;
       // VS
-      let defendGoal = [(this.defense * this.reflex) / 100] - this.fatigue;
+      let goalDefense = [(this.defense * this.reflex) / 100] - this.fatigue;
     }
-    if (attackGoal > defensdGoal) {
-      Team.score += 1;
-    } else {
+    if (goalAttack > goalDefense) {
+      Team.score[0].score += 1;
+    } else if (goalAttack < goalDefense) {
       goalkeeper.hasBall = true;
       this.fatigue += 5;
     }
   };
 }
-/**
+/*
  Classe Équipe 
 ●  nom 
 ●  couleur 
@@ -96,11 +108,84 @@ Composition :
  */
 //                      Team
 export class Team {
-  constructor(name = "", colour = "", players = [], score = 0) {
+  constructor(name = "", colour = "", players = [], score = [{}]) {
     this.name = name;
     this.colour = colour;
     this.players = players;
     this.palmares = [{ playerNb: 0, goal: 0 }];
-    this.score = 0;
+    this.score = [{ Player: Player.name, score: 0 }];
   }
 }
+/*
+|====================================================================|
+|                                                                    |
+|                           Instances                                |
+|                                                                    |
+|====================================================================|
+ */
+export let dribbleResult = Boolean;
+// Teams
+export let team1 = new Team("CastorLovers", "brown", 5, 0);
+export let team2 = new Team("AngryBobers", "red", 5, 0);
+// Players
+export let player1Team1 = new Player("Toupie", 42, 75, 40, 80, 15, [], 0, []); // Attack
+export let player2Team1 = new Player("Saturne", 42, 75, 40, 80, 15, [], 90, []); // Goalkeeper
+export let player3Team1 = new Player("Toto", 42, 75, 40, 80, 15, [], 0, []); // Defense
+export let player4Team1 = new Player("Matcha", 42, 75, 40, 80, 15, [], 0, []); // Attack
+export let player5Team1 = new Player("Snoop", 42, 75, 40, 80, 15, [], 0, []); // Defense
+
+export let player1Team2 = new Player(
+  "Trippi Troppi",
+  42,
+  75,
+  40,
+  42,
+  15,
+  [],
+  0,
+  []
+); // Attack
+export let player2Team2 = new Player(
+  "Bombardiro Crocodillo",
+  42,
+  75,
+  40,
+  40,
+  80,
+  [],
+  0,
+  []
+); // Defense
+export let player3Team2 = new Player(
+  "Tralalero Tralala",
+  42,
+  75,
+  40,
+  80,
+  15,
+  [],
+  0,
+  []
+); // Attacker
+export let player4Team2 = new Player(
+  "Tung Tung Tung Sahur",
+  42,
+  75,
+  40,
+  80,
+  15,
+  [],
+  0,
+  []
+); // Defense
+export let player5Team2 = new Player(
+  "Brr Brr Patapim",
+  42,
+  75,
+  40,
+  80,
+  15,
+  [],
+  0,
+  []
+); // Goalkeeper
--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,29 @@
-/**
+/*
 |====================================================================|
 |                                                                    |
 |                            Imports                                 |
 |                                                                    |
 |====================================================================|
  */
-import { Player, Team } from "./classes.js";
+import {
+  Player,
+  Team,
+  team1,
+  team2,
+  player1Team1,
+  player2Team1,
+  player3Team1,
+  player4Team1,
+  player5Team1,
+  player1Team2,
+  player2Team2,
+  player3Team2,
+  player4Team2,
+  player5Team2,
+  dribbleResult,
+} from "./classes.js";
 
-/**
+/*
 Fonction match(equipe1, equipe2) 
 ●  Création d'un tableau de stats : affiche les buts inscrits par équipe 
 ●  Coup de sifflet départ : la balle est donnée aléatoirement à un attaquant d'une des 
@@ -35,7 +51,35 @@ Fin de match :
 premier objet) 
 ○  Durée : 40 secondes supplémentaires (soit 200 unités) 
  */
-function Match(team1, team2) {
-  let team1Stats = [];
-  let team2Stats = [];
+function match(team1, team2, attacker1, attacker2) {
+  let team1Stats = team1.score[0].score;
+  let team2Stats = team2.score[0].score;
+  console.log(team1Stats, team2Stats);
+
+  let whistle = Math.random(0, 1);
+  whistle = Math.round(whistle);
+  console.log(whistle);
+
+  switch (whistle) {
+    case 0:
+      attacker1.hasBall.push("hasBall");
+      attacker1.dribble(attacker2);
+      if (dribbleResult === true) {
+        console.log(
+          `${attacker1.name} realised a successfull dribble on ${attacker2.name}`
+        );
+      } else {
+        console.log(
+          `${attacker1.name} realised an unsuccessfull dribble on ${attacker2.name} and humiliated themselves`
+        );
+      }
+      break;
+    case 1:
+      attacker2.hasBall.push("hasBall");
+      attacker2.dribble(attacker1);
+      break;
+  }
+  console.log(attacker1.hasBall);
+  console.log(attacker2.hasBall);
 }
+match(team1, team2, player1Team1, player2Team2);
